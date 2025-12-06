49409c8ec7a5877e2b94e6a7c6a4a2b307fc6486
adem <adem@DESKTOP-SEV358T>
2025-12-06 09:35:20 +0100

    fix error

--- config/database.js
+++ config/database.js
@@ -1,13 +1,14 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: String(process.env.DB_PASSWORD),
-  port: Number(process.env.DB_PORT),
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production' ? {
+    rejectUnauthorized: false
+  } : false
 });
+
 
 pool.on('connect', () => {
   console.log('Database connected successfully');
